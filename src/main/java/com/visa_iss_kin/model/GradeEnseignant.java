3eaec324ead435ac2c3f83beea1a3f5454696757
Faustin Flory <pdgfaustin@gmail.com>
2026-06-15 18:02:33 +0100

    GradeEnseignant et StatutPromotionEnseignant créés

---
+++ src/main/java/com/visa_iss_kin/model/GradeEnseignant.java
@@ -0,0 +1,16 @@
+
+package com.visa_iss_kin.model;
+
+/**
+ *
+ * @author Faustin PADINGANYI
+ */
+public enum GradeEnseignant {
+    ASSISTANT_1,
+    ASSISTANT_2,
+    CHEF_TRAVAUX,
+    PROFESSEUR_ASSOCIE,
+    PROFESSEUR,
+    PROFESSEUR_ORDINAIRE,
+    PROFESSEUR_EMERITE
+}
---
+++ src/main/java/com/visa_iss_kin/model/StatutPromotionEnseignant.java
@@ -0,0 +1,13 @@
+
+package com.visa_iss_kin.model;
+
+/**
+ *
+ * @author Faustin PADINGANYI
+ */
+public enum StatutPromotionEnseignant {
+    EN_ATTENTE,
+    VALIDEE,
+    REJETEE,
+    ANNULEE
+}
